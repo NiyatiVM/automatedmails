3d10c166b0b4ff508eb24eb50923ba05f2f9dd66
NiyatiVM <n.mervana@somaiya.edu>
2020-07-05 17:13:03 +0530

    Scheduler

--- cronjob.py
+++ cronjob.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from mailthem import mailthecoders
 scheduler = BlockingScheduler()
-scheduler.add_job(mailthecoders,"interval",hours=3)
+scheduler.add_job(mailthecoders,"interval",hours=24,start_date='2020-07-06 06:00:00')
 scheduler.start()
